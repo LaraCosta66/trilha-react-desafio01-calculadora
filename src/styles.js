d7332ba63cb41e5d494ef0abcf1833d3af58339d
LaraCosta66 <larasmila64@gmail.com>
2022-11-17 12:50:57 -0300

    feat: ajust colors to layout

--- src/styles.js
+++ src/styles.js
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const Container = styled.div`
     width: 100%;
     height: 100vh;
-    background-color: #CACACA;
+    background-color: #C9CDD3;
 
     display: flex;
     align-items: center;
@@ -13,7 +13,7 @@ export const Container = styled.div`
 
 export const Content = styled.div`
     background-color: #FFFFFF;
-    width: 50%;
+    width: 30%;
 `
 
 export const Row = styled.div`
